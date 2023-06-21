3b78f9417b5336697a37bd2b6b3b36487d298922
Leti Student <leti@isu.ru>
2023-06-21 17:31:09 +0800

    Added forming a list

--- roshydro.py
+++ roshydro.py
@@ -3,6 +3,7 @@ from lxml import html
 from lxml import etree
 import os
 import psycopg2
+from datetime import datetime
 
 conn = psycopg2.connect("dbname=reserve user=leti password=leti312 host=localhost")
 
@@ -28,7 +29,7 @@ def get_data(rivernum, firstday):
     return rc
 
 
-rivers = {}
+#rivers = {}
 
 
 def f(v):
@@ -57,6 +58,8 @@ def succ(dt):
         return [y,m+1,d]
     return [y+1,1,d]
 
+dat = {}
+
 def imp(rivernum, fdt, d):
     fdt=fdt[:]
     fdt.reverse()
@@ -65,29 +68,60 @@ def imp(rivernum, fdt, d):
         rc = get_data(1, d)
         for r in rc:
             river = r.attrib["data-river"]
+            if river == "Все реки":
+                continue
+
             # o.write("{}\n".format(river))
-            d = {}
             for riv in r:
                 reserve = riv.text
-                print(river, reserve)
+                # print(river, reserve)
                 # o.write("{}\n".format(reserve))
                 # dt = {}
-                # for a in riv.attrib:
-                #     if a.startswith("water-"):
-                #         v = riv.attrib[a]
-                #         v = list(proc(v))
-                #         a = a.replace("water-", "").replace("-", "_")
-                #         # o.write("{}:{}\n".format(a, v))
-                #         dt[a] = v
-                # d[reserve] = dt
+                try:
+                    for a in riv.attrib:
+                        if a.startswith("water-"):
+                            v = riv.attrib[a]
+                            v = list(proc(v))
+                            a = a.replace("water-", "").replace("-", "_")
+                            # o.write("{}:{}\n".format(a, v))
+                            if a=="date":
+                                # print(fdt)
+                                v=[datetime.strptime(str(k)+'.'+str(fdt[-1]), "%d.%m.%Y") for k in v]
+                                vd = v
+                            elif a=="level":
+                                vl = v
+                    for (d1, l1) in zip(vd, vl):
+                        ll = dat.setdefault((river, reserve, d1),set())
+                        ll.add(l1)
+                except ValueError:
+                    # print("ERROR:", a, v)
+                    pass
             #rivers[river] = d
             # o.write(etree.tostring(riv, encoding=str, pretty_print=True))
         #o.write("{}".format(repr(rivers)))
 
+rivers = {}
+
+def st(k, v):
+    (riv, res, date) = k
+    print(k,v)
+    quit()
+
+def store(dat):
+    cnt = 0
+    for k,v in dat.items():
+        if len(v)>1:
+            print(k,v)
+
+        v=list(v)[0]
+        # if cnt>100:
+        #     break
+        cnt+=1
+        st(k,v)
 
 def impdata(rivernum):
-    startdt=[2017,1,1] # "01-01-2017"
-    enddt=[2023,12,12] # "12-12-2023"
+    startdt=[2013, 6, 1] # "01-06-2013"
+    enddt  =[2023,12,12] # "12-12-2023"
     dt = startdt
     while dt <= enddt:
         d = "{2}-{1}-{0}".format(*dt)
@@ -97,9 +131,13 @@ def impdata(rivernum):
 
 def main():
     # fdt = "01.01.2020"
+    global dat
     impdata(1)
+    # dat=list(dat)
+    # dat.sort()
+    store(dat)
     quit()
-    fdt = os.environ.get("Y","01-01-2023")
+    # fdt = os.environ.get("Y","01-01-2023")
 
 
 if __name__ == '__main__':
